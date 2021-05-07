8a86dad0236540b62f144ddae297dbf495009e99
soronpo <soronpo@tx.technion.ac.il>
2021-05-07 17:17:41 +0300

    remove braces and change import syntax

--- src/main/g8/src/test/scala/Test1.scala
+++ src/main/g8/src/test/scala/Test1.scala
@@ -1,9 +1,6 @@
-
 import org.junit.Test
-import org.junit.Assert._
+import org.junit.Assert.*
 
-class Test1 {
-  @Test def t1(): Unit = {
+class Test1:
+  @Test def t1(): Unit = 
     assertEquals("I was compiled by Scala 3. :)", msg)
-  }
-}
